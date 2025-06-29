1c29c4df40e9fca775190153ee6487d121e4cfb5
lchen7809 <chenleyi7809@outlook.com>
2025-06-29 14:42:09 +0800

    image load

--- quiz-app/next.config.mjs
+++ quiz-app/next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     experimental: {
       appDir: true,      
     },
+    images: {
+      minimumCacheTTL: 60,      
+      formats: ['image/avif','image/webp'],
+    }
   };
   
   export default nextConfig;
